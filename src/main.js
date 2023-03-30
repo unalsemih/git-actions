3b38047488d03eefc5084aa6cea2a5efe6e72f60
unalsemih <semih1837@gmail.com>
2023-03-30 14:54:29 +0300

    Eslint integrated to project

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
-const user1 =  {
+const user1 = {
     name: 'John',
     surname: 'Doe'
 };
 
 const getFullName = function (user){
     return `${user.name} ${user.surname}`;
-}
+};
 
 getFullName(user1);
