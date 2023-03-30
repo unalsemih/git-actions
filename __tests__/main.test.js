26ff20b6cfb17db47e3834393574c2c683501ba1
unalsemih <semih1837@gmail.com>
2023-03-30 17:53:46 +0300

    Coverage integrated to project

--- __tests__/main.test.js
+++ __tests__/main.test.js
@@ -8,3 +8,13 @@ test('it should get full name of user', () => {
 
   expect(user.getFullName(dummyUser)).toBe('John Doe');
 });
+
+test('it should get age of user', () => {
+  const dummyUser = {
+      name: 'Jane',
+      surname: 'Doe',
+      birthYear: 1990,
+  };
+
+  expect(user.getAge(dummyUser)).toBe(33);
+});
