2f7cd517ce941c72ea749e572c7787097229e685
unalsemih <semih1837@gmail.com>
2023-04-01 00:45:14 +0300

    tests and coveralls action integrated

--- src/user.js
+++ src/user.js
@@ -6,6 +6,10 @@ const getAge = function (user){
     return new Date().getFullYear() - user.birthYear;
 };
 
+const getAge2 = function (user){
+    return new Date().getFullYear() - user.birthYear;
+};
+
 module.exports = {
     getFullName,
     getAge
